6fce69972a2219cf87c21e7da795497d25aa6ad7
Salma <salmaaly47@gmail.com>
2024-12-01 12:57:16 -0800

    Integrated backend for generating list of restuarants

--- frontend/src/pages/Restaurants.tsx
+++ frontend/src/pages/Restaurants.tsx
@@ -1,17 +1,38 @@
-import React from "react";
-import RestaurantList, { RestaurantItem } from "../components/RestaurantList";
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
 
-export default function RestaurantsPage() {
-  const restaurants: RestaurantItem[] = [
-    { id: 1, name: "Restaurant A" },
-    { id: 2, name: "Restaurant B" },
-    { id: 3, name: "Restaurant C" },
-  ];
+export default function Restaurants() {
+  const [restaurants, setRestaurants] = useState([]);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    // Update with whatever server is running backend
+    // Command to run server: python -m backend --port=3500
+    axios.get("http://localhost:3500/search")
+      .then((response) => {
+        setRestaurants(response.data);
+      })
+      .catch((error) => {
+        console.error("Error fetching restaurants:", error);
+      });
+  }, []);
+
+  const handleRestaurantClick = (name: string) => {
+    navigate(`/restaurant/${name}`, { state: { name } });
+  };
 
   return (
     <div>
-      <h1>Restaurants near UW</h1>
-      <RestaurantList items={restaurants} />
+        <h1>Restaurants near UW</h1>
+        {restaurants.map((restaurant) => (
+        <div
+          key={restaurant}
+          onClick={() => handleRestaurantClick(restaurant)}
+        >
+          <h2>{restaurant}</h2>
+        </div>
+      ))}
     </div>
   );
 }
